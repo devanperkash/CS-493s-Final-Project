change to kl-div loss for logits and fix generate_text methods

scripts/models.py
@@ -28,7 +28,7 @@ class TeacherModel():
 
         return sequences, logits
 
-    def get_teacher_y(self, input_text, max_length=50, remove_additional_tokens=False):
+    def get_teacher_y(self, input_text, max_length=None, remove_additional_tokens=True):
         sequences, logits = self._run_teacher_model(input_text, max_length)
         logits = torch.stack(logits, dim=1)[:, :, :len(self.tokenizer)]  if remove_additional_tokens else torch.stack(logits, dim=1) 
 
@@ -37,7 +37,7 @@ class TeacherModel():
     def generate_text(self, input_text, max_length=50):
         sequences, _ = self._run_teacher_model(input_text, max_length)
 
-        return self.tokenizer.decode(sequences[0], skip_special_tokens=True)
+        return [self.tokenizer.decode(sequence, skip_special_tokens=True) for sequence in sequences]
 
 class StudentModel(nn.Module):
     def __init__(self, teacher_tokenizer, emb_dim=512, n_heads=8, n_layers=4, ff_dim=2048, dropout=0.1):
@@ -78,7 +78,7 @@ class StudentModel(nn.Module):
             next_token_id = torch.argmax(next_token_logits, dim=-1, keepdim=True)
             generated_ids = torch.cat([generated_ids, next_token_id], dim=1)
 
-        sequences = generated_ids[0]
+        sequences = generated_ids
         logits = all_logits
 
         return sequences, logits
@@ -94,4 +94,4 @@ class StudentModel(nn.Module):
             next_token_id = torch.argmax(next_token_logits, dim=-1, keepdim=True)
             generated_ids = torch.cat([generated_ids, next_token_id], dim=1)
 
-        return self.tokenizer.decode(generated_ids[0], skip_special_tokens=True)
+        return [self.tokenizer.decode(sequence, skip_special_tokens=True) for sequence in generated_ids]

scripts/train.py
@@ -1,4 +1,4 @@
-from dataloader import load_hellaswag
+from dataloader import load_hellaswag, load_gsm8k
 from distill_utils import get_device, count_parameters
 from models import TeacherModel, StudentModel
 import torch.nn as nn
@@ -9,10 +9,11 @@ def train_student(logits_distillation:bool = False):
     # print(f"Running on: {device}")
 
     # Load data
-    hellaswag_subset_train, hellaswag_subset_val = load_hellaswag(3)
+    #subset_train, subset_val = load_hellaswag(3)
+    subset_train, subset_val = load_gsm8k(3)
 
     # Get models setup
-    teacher_model = TeacherModel("deepseek-ai/deepseek-coder-1.3b-base")
+    teacher_model = TeacherModel("deepseek-ai/deepseek-coder-1.3b-base") #deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B
     student_model = StudentModel(teacher_model.tokenizer)
 
     print('Vocab length:', len(teacher_model.tokenizer))
@@ -20,13 +21,13 @@ def train_student(logits_distillation:bool = False):
     count_parameters(student_model)
 
     # Prep for training
-    loss_func = nn.CrossEntropyLoss()
+    loss_func = nn.KLDivLoss()
     optimizer = optim.Adam(student_model.parameters())
 
     # Run models
     for i in range(5):
-        t_seqs, t_logits = teacher_model.get_teacher_y(hellaswag_subset_train['ctx'], remove_additional_tokens=True)
-        s_seqs, s_logits = student_model.get_student_y_hat(hellaswag_subset_train['ctx'])
+        t_seqs, t_logits = teacher_model.get_teacher_y(subset_train)
+        s_seqs, s_logits = student_model.get_student_y_hat(subset_train)
 
         optimizer.zero_grad()
         loss = loss_func(t_logits, s_logits)
@@ -36,11 +37,22 @@ def train_student(logits_distillation:bool = False):
         print(i, loss)
 
         print("Example:")
+        print(teacher_model.generate_text(subset_val[0]))
         print("---")
-        print(student_model.generate_text(hellaswag_subset_train['ctx']))
+        print(student_model.generate_text(subset_val[0]))
         print("---")
+
+def test():
+    subset_train, subset_val = load_gsm8k(3)
+
+    teacher_model = TeacherModel("deepseek-ai/deepseek-coder-1.3b-base")
+    student_model = StudentModel(teacher_model.tokenizer)
+    z = teacher_model.generate_text(["What is your name?", "What is 9+9?"])
+    print(z)
+
 
 
 if __name__ == "__main__":
-    train_student(logits_distillation=True)
+    #train_student(logits_distillation=True)
+    test()
 